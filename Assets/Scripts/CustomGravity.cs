b9e8e1fee1bf93d812ecc96e1ce0a5fbff89ccc7
Guebeen <211shyn@gmail.com>
2025-01-27 21:34:07 +0900

    [feat]: GPause 2차구현
    바뀐 구조에 맞춰서 GPause 2차구현

--- Assets/Scripts/CustomGravity.cs
+++ Assets/Scripts/CustomGravity.cs
@@ -47,9 +47,42 @@ public class CustomGravity : MonoBehaviour
 
     public void ReapplyGravity()
     {
+<<<<<<< Updated upstream
         /*
          * Reinitialize customGravity
          */
         customGravity = Physics.gravity;
+=======
+        if(GameManager.instance.isSideView)
+            SetToConvert();
+        else
+        {
+            if(_stateBeforeConvert == GravityState.defaultG)
+                SetToDefault();
+            else
+                SetToInvert();
+        }
+    }
+
+    public void GPause()
+    {
+        //GPause 활성화될때 호출
+        if (gravityState == GravityState.defaultG)
+            gPauseAtDefaultEvent?.Invoke();
+        else if (gravityState == GravityState.invertG)
+            gPauseAtInvertEvent?.Invoke();
+    }
+
+    private void CallGPauseAction()
+    {
+        StartCoroutine(GPauseAction());
+    }
+
+    IEnumerator GPauseAction()
+    {
+        InvertAction();
+        yield return new WaitForSeconds(10f);
+        InvertAction();
+>>>>>>> Stashed changes
     }
 }
