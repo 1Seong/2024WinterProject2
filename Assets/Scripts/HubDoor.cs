ab617556407b79082a9af9167727840db6a19387
leo5852-1 <leokyj5852@gmail.com>
2025-07-30 17:18:41 +0900

    [feat]Hub Stage 구현 중
    HubDoor script의 lock 관련 로직 수정 필요

---
+++ Assets/Scripts/HubDoor.cs
@@ -0,0 +1,109 @@
+using System.Runtime.CompilerServices;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.ProBuilder.MeshOperations;
+using static PlayerSelectableInterface;
+
+public class HubDoor : MonoBehaviour
+    //TODO: Door.cs와 상속 관련 refactor 고려
+
+{
+    enum PlayerColor { pink = 1, blue }
+    [SerializeField] private PlayerColor color;
+    [SerializeField] public Episode ep;
+    [SerializeField] public int stage;
+    //[SerializeField] private HubDoor[] blueDoors;
+    [SerializeField] private HubDoor[] pinkDoors;
+    
+    private bool locked = false;
+    private bool epSelected = false;
+    private GrayScript gs;
+
+    PlayerSelectableInterface playerSelectable = new PlayerSelectable();
+
+    private float goalTime = 2.0f;
+    private float enterTime, stayTime;
+
+    private void Awake()
+    {
+       gs = GetComponent<GrayScript>();
+       if (gs != null)
+            gs.turnGray();
+       else
+            Debug.LogWarning("GrayScript Not Found");
+    }
+    private void Start()
+    {
+        // 문이 핑크색이거나 아직 해금 안되었다면 잠근다
+        if (color == PlayerColor.pink) lockDoor();
+        else if (!DataManager.Instance.getIsUnlocked(ep, stage))
+        {
+            lockDoor();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (playerSelectable.CheckColor(other, (int)color) == false) return;
+        enterTime = Time.time;
+        if( color == PlayerColor.blue )
+        {
+            for (int i = 0; i < 5; i ++)
+            {
+                if (pinkDoors.Length != 5) return;
+                pinkDoors[i].ep = ep;
+                pinkDoors[i].stage = i;
+                if (DataManager.Instance.getIsUnlocked(ep, i)) 
+                {
+                    pinkDoors[i].unlockDoor();
+                }
+                else 
+                    pinkDoors[i].lockDoor();
+            }
+                
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (locked) return; 
+        if (color == PlayerColor.pink && !epSelected) return;
+        if (playerSelectable.CheckColor(other, (int)color) == false) return;
+
+        stayTime = Time.time - enterTime;
+        if (stayTime >= goalTime)
+        {
+            GetComponent<Collider>().enabled = false;
+            other.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+            other.GetComponent<Transparent>().CallFade();
+            //StageManager.instance.CheckStageClear();
+
+            if(color == PlayerColor.blue)
+            {
+                foreach ( HubDoor door in pinkDoors)
+                {
+                    door.epSelected = true;
+                }
+            }
+            else if(color == PlayerColor.pink)
+            {
+                StageManager.instance.StageEnter(ep, stage);
+            }
+        }
+    }
+
+    private void lockDoor()
+    {
+        Debug.Log(ep.ToString() + " " + stage.ToString() + " Locked");
+        if (locked) return;
+        locked = true;
+        gs.turnGray();
+    }
+    private void unlockDoor()
+    {
+        if (!locked) return;
+        Debug.Log(ep.ToString() + " " + stage.ToString() + " Unlocked");
+        locked = false;
+        gs.turnDeGray();
+    }
+}
---
+++ Assets/Shader/GrayScript.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using UnityEngine;
+
+public class GrayScript : MonoBehaviour
+{
+    public Material mat;
+    public float duration = 1.0f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        mat = GetComponentInChildren<SpriteRenderer>().material;
+        //turnGray();
+    }
+
+    public void turnGray()
+    {
+       // Debug.Log("gray start!");
+        StartCoroutine(GrayRoutine());
+    }
+
+    public void turnDeGray()
+    {
+        //Debug.Log("degray start!");
+        StartCoroutine(deGrayRoutine());
+    }
+
+    private IEnumerator GrayRoutine()
+    {
+        float t = duration;
+        while (t > 0.0f)
+        {
+            mat.SetFloat("_GrayAmount", t);
+            t -= Time.deltaTime;
+            yield return null;
+        }
+
+        mat.SetFloat("_GrayAmount", 0);
+        //Debug.Log("gray complete!");
+    }
+
+    private IEnumerator deGrayRoutine()
+    {
+        float t = 0;
+        while (t < duration)
+        {
+            mat.SetFloat("_GrayAmount", t);
+            t += Time.deltaTime;
+            yield return null;
+        }
+
+        mat.SetFloat("_GrayAmount", 1);
+        //Debug.Log("degray complete!");
+    }
+}
