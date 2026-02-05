ced366b8d15d2d20f236ab80cd3f06515ebedb9f
SEONG WON CHOI <qwik0102@gmail.com>
2026-02-05 20:53:14 +0900

    [fix] : 배경 관련 수정

--- Assets/Scripts/Items/ItemFloating.cs
+++ Assets/Scripts/Items/ItemFloating.cs
@@ -5,16 +5,19 @@ public class ItemFloating : MonoBehaviour
 {
     public float FloatDistance = 0.1f;
     public float Speed = 1.2f;
+    public bool IsForward = true;
     private Vector3 basePos;
+    private Vector3 baseDir;
 
     private void Start()
     {
+        baseDir = IsForward ? transform.forward : transform.up;
         basePos = transform.position;
     }
 
     void Update()
     {
         float offset = Mathf.Sin(Time.time * Speed) * FloatDistance;
-        transform.position = basePos + transform.forward * offset;
+        transform.position = basePos + baseDir * offset;
     }
 }
